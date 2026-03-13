b1169fd8d7f6dbc3a5c299981eb8d178303c2c90
Lion d'Oc <mathias.szanto@gmail.com>
2026-03-13 11:38:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -560,5 +560,5 @@ with app.app_context():
     init_db()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
